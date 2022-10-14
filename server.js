1c6b0e5f3d6a73059c2d279955bd4d5e9f77940c
Leah Chercham <leahchercham@hotmail.com>
2022-10-14 18:38:01 +0200

    lg

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.use('/', user)
 app.use('/', messenger)
 app.use('/', offer)
 
+console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI
   || 'mongodb://127.0.0.1:27017/KeepGoingDB',
   { useNewUrlParser: true })
