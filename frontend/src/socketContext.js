78f21ec9e9f45bcfad16e7b2f9c462b09427cd6e
Leah Chercham <leahchercham@hotmail.com>
2022-10-14 18:23:48 +0200

    test

--- frontend/src/socketContext.js
+++ frontend/src/socketContext.js
@@ -3,14 +3,15 @@ import React from 'react';
 
 // export const socket = io('ws://localhost:8000');
 
+let socketurl = window.location.hostname === "localhost"
+    ? "http://127.0.0.1:5000"
+    : "https://keepgoingapp.herokuapp.com/"
 
-export const socket = window.location.host.includes('localhost') ?
-    io(`ws://localhost:5000`)
-    : io(`ws://keepgoingapp.herokuapp.com`)
+export const socket = io.connect(socketurl)
 
 // export const socket = window.location.host.includes('localhost') ?
 // io(`ws://localhost:5000`)
 // : io(`ws://keepgoingapp.herokuapp.com:${process.env.PORT}`)
 
 
-// export const SocketContext = React.createContext();
+export const SocketContext = React.createContext();
